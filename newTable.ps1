27fb62bc512e111cf3374a4ca1a088526eb10bfa
bitlasowjanya <33808371+bitlasowjanya@users.noreply.github.com>
2017-11-19 12:48:19 -0500

    creating table storage

---
+++ newTable.ps1
@@ -0,0 +1,15 @@
+﻿function table(){
+
+    param(
+        [Parameter(Mandatory=$True,Position=1)]
+        [string]$Rgp,
+        [Parameter(Mandatory=$True,Position=2)]
+        [string]$stgAcc,
+        [Parameter(Mandatory=$True,Position=3)]
+        [string]$tableName
+        )
+
+Get-AzureRmStorageAccount -ResourceGroupName "$Rgp" -Name "$stgAcc" | `
+New-AzureStorageTable -Name "$tableName" 
+}
+table -Rgp "myTestRg003" -stgAcc "mysamplestoacc002" -tableName "table002"
