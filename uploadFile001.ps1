c7af960dd427179370611161bb1453deb7d653db
bitlasowjanya <sowjanya.bitla@gmail.com>
2017-11-19 11:20:09 -0500

    upload file

---
+++ uploadFile001.ps1
@@ -0,0 +1,26 @@
+﻿
+
+
+function uploadBlob(){
+
+    param(
+        [Parameter(Mandatory=$True,Position=1)]
+        [string]$Rgp,
+        [Parameter(Mandatory=$True,Position=2)]
+        [string]$stgAcc,
+        [Parameter(Mandatory=$True,Position=3)]
+        [string]$container,
+        [Parameter(Mandatory=$True,Position=4)]
+        [string]$fileName
+        )
+
+
+    Get-AzureRmStorageAccount -ResourceGroupName "$Rgp" -Name "$stgAcc" | `
+    New-AzureStorageContainer -Name "$container" -Permission Blob | `
+    Set-AzureStorageBlobContent -Container "$container" -File "$fileName" -BlobType Block
+
+
+}
+
+
+uploadBlob -Rgp "myTestRg003" -stgAcc "mysamplestoacc002" -container "mytestcontainer004" -fileName "C:\D\sowjanya\testxml.xml"
