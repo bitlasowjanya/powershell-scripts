376302bd77e874b6d3575384203adcbbe9d2add5
bitlasowjanya <sowjanya.bitla@gmail.com>
2017-11-19 12:42:39 -0500

    before modify

---
+++ uploadfile002.ps1
@@ -0,0 +1,21 @@
+﻿function uploadBlob(){
+
+    param(
+        [Parameter(Mandatory=$True,Position=1)]
+        [string]$Rgp,
+        [Parameter(Mandatory=$True,Position=2)]
+        [string]$stgAcc,
+        [Parameter(Mandatory=$True,Position=3)]
+        [string]$container,
+        [Parameter(Mandatory=$True,Position=4)]
+        [string]$fileName
+        )
+
+
+    Get-AzureRmStorageAccount -ResourceGroupName "$Rgp" -Name "$stgAcc" | `
+    Get-AzureStorageContainer -Name "$container" | `
+    Set-AzureStorageBlobContent -Container "$container" -File "$fileName" -BlobType Block
+
+
+}
+uploadBlob -Rgp "myTestRg003" -stgAcc "mysamplestoacc002" -container "mytestcontainer002" -fileName "C:\D\aws-login.ppk"
