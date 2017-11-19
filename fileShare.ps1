1d9aa209d59af68f5479b88d86fd05266d5f2592
bitlasowjanya <33808371+bitlasowjanya@users.noreply.github.com>
2017-11-19 12:46:46 -0500

    creating file share

---
+++ fileShare.ps1
@@ -0,0 +1,18 @@
+﻿
+function fileShare(){
+
+    param(
+        [Parameter(Mandatory=$True,Position=1)]
+        [string]$Rgp,
+        [Parameter(Mandatory=$True,Position=2)]
+        [string]$stgAcc,
+        [Parameter(Mandatory=$True,Position=3)]
+        [string]$fileShare
+        )
+
+Get-AzureRmStorageAccount -ResourceGroupName "$Rgp" -Name "$stgAcc" | ` 
+New-AzureStorageShare -Name "$fileShare"
+}
+
+
+fileShare -Rgp "myTestRg003" -stgAcc "mysamplestoacc002" -fileShare "testfile"
