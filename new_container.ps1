77407ce6dd7936c2e750802051f444b4766dfa39
bitlasowjanya <33808371+bitlasowjanya@users.noreply.github.com>
2017-12-21 13:48:13 -0500

    creating new container

---
+++ new_container.ps1
@@ -0,0 +1,16 @@
+﻿function new_container(){
+
+    param(
+        [Parameter(Mandatory=$True,Position=1)]
+        [string]$rgp,
+        [Parameter(Mandatory=$True,Position=2)]
+        [string]$stg_acc,
+        [Parameter(Mandatory=$True,Position=3)]
+        [string]$container_name
+         )
+
+Get-AzureRmStorageAccount -ResourceGroupName $rgp -Name $stg_acc | ` 
+New-AzureStorageContainer -Name $container_name -Permission Blob 
+}
+
+new_container -rgp "myTestRg003" -stg_acc "data002" -container_name "testcontainer006" 
