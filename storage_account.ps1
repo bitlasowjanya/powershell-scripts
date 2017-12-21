79437ce91c0c883b4c1fb17ba20cd15127b5b3d7
bitlasowjanya <33808371+bitlasowjanya@users.noreply.github.com>
2017-12-21 13:50:06 -0500

    create new storage account

---
+++ storage_account.ps1
@@ -0,0 +1,16 @@
+﻿function storage_account(){
+
+    param(
+        [Parameter(Mandatory=$True,Position=1)]
+        [string]$rgp,
+        [Parameter(Mandatory=$True,Position=2)]
+        [string]$stg_name,
+        [Parameter(Mandatory=$True,Position=3)]
+        [string]$location
+         )
+
+
+    New-AzureRmStorageAccount $rgp -Name $stg_name -SkuName Standard_LRS -Location $location -Kind Storage 
+}
+
+storage_account -rgp "myTestRg003" -stg_name "data002" -location "central us"
